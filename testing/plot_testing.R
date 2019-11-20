0c7853236d498f49f5a06a136325b8744141a173
Jonas Moss <jonas.gjertsen@gmail.com>
2019-11-20 19:46:17 +0100

    :bird: Removed indentation.

--- testing/plot_testing.R
+++ testing/plot_testing.R
@@ -1,16 +1,15 @@
+plot_test = function(index, N = 10000) {
 
-  plot_test = function(index, N = 10000) {
+  N = 10000
+  data = rjh(N, index)
+  x = seq(0, 1, by = 0.001)
+  hist(rjh(N, index), breaks = 100, freq = FALSE)
+  lines(x, djh(x, index))
 
-    N = 10000
-    data = rjh(N, index)
-    x = seq(0, 1, by = 0.001)
-    hist(rjh(N, index), breaks = 100, freq = FALSE)
-    lines(x, djh(x, index))
+  plot(ecdf(data))
+  lines(x, pjh(x, index), col = "red")
 
-    plot(ecdf(data))
-    lines(x, pjh(x, index), col = "red")
+}
 
-  }
-
-  plot_test(1)
-  plot_test(15)
+plot_test(1)
+plot_test(15)
